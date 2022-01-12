5bd7af0efd75335ff51bb32d4a3622a56baed68a
NONP <zen.cm@traditionasia.com>
2022-01-12 12:09:56 +0700

    update new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {HeroDetailComponent} from './hero-detail/hero-detail.component';
 import {HeroesComponent} from './heroes/heroes.component';
 import {MessagesComponent} from './messages/messages.component';
 import {HeroSearchComponent} from "./hero-search/hero-search.component";
+import {HelloComponent} from './hello/hello.component';
 
 @NgModule({
   imports: [
@@ -35,7 +36,8 @@ import {HeroSearchComponent} from "./hero-search/hero-search.component";
     HeroesComponent,
     HeroDetailComponent,
     MessagesComponent,
-    HeroSearchComponent
+    HeroSearchComponent,
+    HelloComponent
   ],
   bootstrap: [AppComponent]
 })
